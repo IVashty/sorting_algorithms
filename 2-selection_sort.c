2e7ec474c711b2831edc2951552985f80f3c8bdc
IVashty <vashtylavilavi50@gmail.com>
2022-11-08 07:21:28 +0000

    Added my swap function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,20 @@
 #include "sort.h"
 /**
+ * swap - function that swap array elements
+ * @n_one: ptr to array element
+ * @n_two: prt to array element
+ * Return: void
+ */
+
+void swap(int *n_one, int *n_two)
+{
+int temp = *n_one;
+*n_one = *n_two;
+*n_two = temp;
+}
+
+
+/**
   * selection_sort - algorithm. slow.
   * @array: input array
   * @size: size of the input
